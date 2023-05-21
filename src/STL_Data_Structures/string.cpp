399bcca6631b77025e1a6c1573560d9a985ffd66
Sriteja Pashya <sritejapashya@gmail.com>
2023-05-21 17:37:56 +0530

    added empty files

---
+++ src/STL_Data_Structures/string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  string s;
+  cin >> s;
+  cout << "s = " << s << endl;
+  
+  
+  
+  return 0;
+}
